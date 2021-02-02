41ca2b6951ff3bdd441e6622fd1dfe3b96517288
Lucas Strand <lucas.strand@outlook.com>
2021-02-02 14:34:09 +0100

    Added fundamentals, index not working

---
+++ app.js
@@ -0,0 +1,20 @@
+const { response, Router, request } = require('express')
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine(".hbs", expressHandlebars({
+  defaultLayout: "main.hbs"
+}))
+
+app.use(express.static("static"))
+app.use(express.static("./public/images"))
+
+app.get('/', function(request, response){
+  response.render("index.hbs")
+})
+
+app.listen(8080, function() {
+  console.log("listen port 8080")
+})
