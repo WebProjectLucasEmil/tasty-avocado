22e39c700b9c6d202497f2041d30cb59998957cf
Lucas Strand <lucas.strand@outlook.com>
2021-02-08 14:18:17 +0100

    Css for login added

--- calendify/src/app.js
+++ calendify/src/app.js
@@ -1,6 +1,10 @@
 const { response, Router, request } = require('express')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const expressSession = require('express-session')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs')
+const token = require('csurf')
 // const nodemon = require('nodemon')
 
 const app = express()
@@ -16,8 +20,12 @@ app.set("views", "src" + "/views")
 
 app.use(express.static("./public/images"))
 
-app.get('/', function(request, response){
-  response.render("index.hbs")
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+app.get("/", csrfProtection, function (request, response) {
+  response.render("index.hbs", {token: request.csrfToken()})
 })
 
 app.listen(8080, function() {
