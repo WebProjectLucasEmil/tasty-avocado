7637ea6183d5095f640c23e155d0ef1728c2b7f7
Lucas Strand <lucas.strand@outlook.com>
2021-02-09 13:24:53 +0100

    added admin login, needs to put into appropiate layers

--- calendify/src/app.js
+++ calendify/src/app.js
@@ -9,8 +9,8 @@ const sqlite3 = require("sqlite3")
 const SQLiteStore = require('connect-sqlite3')(expressSession)
 // const nodemon = require('nodemon')
 
-const csrfProtection = token({ cookie: false})
-const parseForm = bodyParser.urlencoded ({extended: false})
+const csrfProtection = token({ cookie: false })
+const parseForm = bodyParser.urlencoded({ extended: false })
 
 const app = express()
 
@@ -23,8 +23,10 @@ app.use(expressSession({
   })
 }))
 
+//! DON'T FORGET TO MOVE THIS TO APPROPIATE LOG IN LAYER(if it isn't already, kek)--------------
 const adminUsername = "admin"
 const adminPassword = "$2a$10$.WNk7GjUq5cBvbWbuXVO5Ok8ksPm4y5TTLZOY3GajRXC.ECn6PyZ6"
+//!--------------------------------------------------------------------------------------
 
 app.engine(".hbs", expressHandlebars({
   defaultLayout: "main.hbs"
@@ -40,10 +42,41 @@ app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-app.get("/", csrfProtection, function (request, response) {
-  response.render("index.hbs", {token: request.csrfToken()})
+app.use(function (request, response, next) {
+  const isLoggedIn = request.session.isLoggedIn
+
+  response.locals.isLoggedIn = isLoggedIn
+  next()
 })
 
-app.listen(8080, function() {
+app.get("/", csrfProtection, function (request, response) {
+  response.render("index.hbs", { token: request.csrfToken() })
+})
+
+app.post("/", csrfProtection, parseForm, function (request, response) {
+  //TODO: Re-structure login to appropiate layer, also only admin atm. Needs to be fixed as well.
+  const enteredUsername = request.body.username
+  const enteredPassword = request.body.password
+
+  const checkPassword = bcrypt.compareSync(enteredPassword, adminPassword)
+
+  if (enteredUsername == adminUsername && checkPassword) {
+    //Login
+    request.session.isLoggedIn = true
+    console.log("never gonna give you up...")
+    response.redirect("/")
+  } else {
+    //TODO:display error message to user
+  }
+})
+
+app.post("/logout", csrfProtection, parseForm, function (request, response) {
+  request.session.isLoggedIn = false
+  console.log("never gonna let you down...")
+  response.redirect("/")
+})
+
+
+app.listen(8080, function () {
   console.log("listen port your mom")
 })
---
+++ calendify/src/presentationLayer/login-router.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+// const expressSession = require('express-session')
+// const bcrypt = require('bcryptjs')
+// const router = express.Router()
+
+
+
+// const enteredUsername = request.body.username
+// const enteredPassword = request.body.password
+
+// router.get("/", function(req, res){
+  
+// })
+// // const checkPassword = bcrypt.compareSync(enteredPassword, adminPassword)
+
+// // if (enteredUsername == adminUsername && checkPassword) {
+// //   //Login
+// //   request.session.isLoggedIn = true
+// //   response.redirect("/")
+// // } else {
+// //   //display error message to user
+// // }
+
