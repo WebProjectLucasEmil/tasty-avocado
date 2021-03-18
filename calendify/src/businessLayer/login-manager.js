4ce92f2b5aec639a3248a2574503a037be7ceb42
Lucas Strand <lucas.strand@outlook.com>
2021-03-18 13:52:29 +0100

    Rediz added

--- calendify/src/businessLayer/login-manager.js
+++ calendify/src/businessLayer/login-manager.js
@@ -4,15 +4,17 @@ const { response, Router, request } = require('express')
 const loginRepository = require("../dataAccessLayer/login-repository")
 
 
-function loginMan(enteredUsername, enteredPassword, isLoggedIn){
+function loginMan(enteredUsername, enteredPassword){
     //TODO: Re-structure login to appropiate layer, also only admin atm. Needs to be fixed as well.
-    const adminUsername = "admin"
-    const adminPassword = "$2a$10$.WNk7GjUq5cBvbWbuXVO5Ok8ksPm4y5TTLZOY3GajRXC.ECn6PyZ6"
-    const checkPassword = bcrypt.compareSync(enteredPassword, adminPassword)
+    const adminUsername = loginRepository.getAdminUsername()
+    const adminPassword = loginRepository.getAdminPassword()
+    // const adminPassword = "$2a$10$.WNk7GjUq5cBvbWbuXVO5Ok8ksPm4y5TTLZOY3GajRXC.ECn6PyZ6"
+    // const checkPassword = bcrypt.compareSync(enteredPassword, adminPassword)
  
-    if (enteredUsername == adminUsername && checkPassword) {
+    // if (enteredUsername == adminUsername && checkPassword) {
+      if (enteredUsername == adminUsername && enteredPassword == adminPassword){
       //Login
-      //isLoggedIn = true
+      ////isLoggedIn = true
       console.log("never gonna give you up...")
       response.redirect("/")
     } else {
